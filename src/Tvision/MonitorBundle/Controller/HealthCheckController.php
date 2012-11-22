503db7a7a4bd5436345c87bd1db92e93906f951a
Gianluca Bargelli <g.bargelli@gmail.com>
2012-11-22 09:24:55 +0100

    Added exception

--- src/Tvision/MonitorBundle/Controller/HealthCheckController.php
+++ src/Tvision/MonitorBundle/Controller/HealthCheckController.php
@@ -25,7 +25,7 @@ class HealthCheckController extends Controller
 
         foreach ($results as $id => $result) {
             $tmp = $result->toArray();
-            if ($tmp['status'] > CheckResult::WARNING) {
+            if ($tmp['status'] > CheckResult::WARNING && !preg_match("/jig.terravision.eu/", $tmp['checkName'])) {
                 $globalStatus .= 'KO | ' . $id . '. ' . $tmp['checkName'] . ':' . $tmp['message'] ."\n";
             }
         }
